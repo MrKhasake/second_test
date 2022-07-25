bf56d175843ae0336536dd324407a814ae01f0fc
CFE02 <CFELAB02@GoldfieldsTVET.Edu.ZA>
2022-07-25 15:45:51 +0200

    first commit

---
+++ bona/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Entry
+from .forms import EntryForm
+
+
+def index(request):
+    entries = Entry.objects.order_by('-date_posted')
+
+    context = {'entries' : entries}
+    return render(request, 'entries/index.html', context)
+
+def add(request):
+    form = EntryForm()
+
+    context = {'form' : form}
+
+    return render(request, 'entries/add.html', context)
+
